78962b0b578af292930cc71e791774cb0300637e
Martin Resch <resch@lonetech.de>
2022-03-31 23:23:51 +0200

    breakpoint

--- src/main/java/de/lonetech/mongodbopenshiftdemo/MongodbOpenshiftDemoApplication.java
+++ src/main/java/de/lonetech/mongodbopenshiftdemo/MongodbOpenshiftDemoApplication.java
@@ -1,6 +1,5 @@
 package de.lonetech.mongodbopenshiftdemo;
 
-import de.lonetech.mongodbopenshiftdemo.model.Transaction;
 import de.lonetech.mongodbopenshiftdemo.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -21,11 +20,12 @@ public class MongodbOpenshiftDemoApplication
 
   void createTransactions() {
     System.out.println("Data creation started...");
-    transactionRepo.save(new Transaction("Whole Wheat Biscuit", "Whole Wheat Biscuit", 5, "snacks"));
-    groceryItemRepo.save(new GroceryItem("Kodo Millet", "XYZ Kodo Millet healthy", 2, "millets"));
-    groceryItemRepo.save(new GroceryItem("Dried Red Chilli", "Dried Whole Red Chilli", 2, "spices"));
-    groceryItemRepo.save(new GroceryItem("Pearl Millet", "Healthy Pearl Millet", 1, "millets"));
-    groceryItemRepo.save(new GroceryItem("Cheese Crackers", "Bonny Cheese Crackers Plain", 6, "snacks"));
+    // BREAKPOINT
+//    transactionRepo.save(new Transaction("Whole Wheat Biscuit", "Whole Wheat Biscuit", 5, "snacks"));
+//    groceryItemRepo.save(new GroceryItem("Kodo Millet", "XYZ Kodo Millet healthy", 2, "millets"));
+//    groceryItemRepo.save(new GroceryItem("Dried Red Chilli", "Dried Whole Red Chilli", 2, "spices"));
+//    groceryItemRepo.save(new GroceryItem("Pearl Millet", "Healthy Pearl Millet", 1, "millets"));
+//    groceryItemRepo.save(new GroceryItem("Cheese Crackers", "Bonny Cheese Crackers Plain", 6, "snacks"));
     System.out.println("Data creation complete...");
   }
 
